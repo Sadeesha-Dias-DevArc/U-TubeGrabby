07fa2d9225a4cdd0a946cf8ba0c0d99bf61d75f8
Sadeesha-Dias-DevArc <sadeesha.dias.sd6@gmail.com>
2024-02-11 12:50:01 +0530

    Adding UI elements

--- src/main.py
+++ src/main.py
@@ -11,5 +11,19 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("U-TubeGrabby")
 
+# UI elements
+# Adding URL lable
+url_label = customtkinter.CTkLabel(app, text="Copy and paste the YouTube URL")
+url_label.pack(padx=10, pady=10)
+
+#  Adding URL entry field
+link_var = tkinter.StringVar()
+url_entry_field = customtkinter.CTkEntry(app, width=350, height=40, textvariable=link_var)
+url_entry_field.pack(padx=10, pady=10)
+
+# Adding download button
+download_button = customtkinter.CTkButton(app, text="Analyze")
+download_button.pack(padx=10, pady=10)
+
 # Run application
 app.mainloop()
