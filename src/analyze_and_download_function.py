4249979f1f40f6488a38a1bcc69a0952b8276279
Sadeesha-Dias-DevArc <sadeesha.dias.sd6@gmail.com>
2024-02-11 14:31:24 +0530

    bug fixed - Printing "Download Complete" regardless the if-else condition check

--- src/analyze_and_download_function.py
+++ src/analyze_and_download_function.py
@@ -5,6 +5,7 @@ def startDownload(url):
         youtube_Obj = YouTube(url)  # Create a YouTub object from the url   
         video = youtube_Obj.streams.get_highest_resolution()   # Get the highest resolution video stream  
         video.download('C:/Users/User/OneDrive/Desktop/Sadeesha-Dias-DevArc Projects/U-TubeGrabby/videos')  # Download the video to defined folder
+        print("Download Complete!")
     except:
         print("There was an error in the URL!")
-    print("Download Complete!")
+    
