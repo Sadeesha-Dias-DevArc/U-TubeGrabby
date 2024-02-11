a9e90666e0f392f83a27d6e64c941e334651076c
Sadeesha-Dias-DevArc <sadeesha.dias.sd6@gmail.com>
2024-02-11 12:26:19 +0530

    Creating initial application GUI structure

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
-print("Hello")
-print("there! dev")
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+# Application window settings
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+# Application window frame settings
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("U-TubeGrabby")
+
+# Run application
+app.mainloop()
